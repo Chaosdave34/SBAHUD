d4767a4c05d69038132ceddb5c2fc532b585ff17
David <chaosdave34@users.noreply.github.com>
2024-01-03 19:49:28 +0100

    Changed version from 0.1 to 0.1.0

--- src/main/java/io/github/chaosdave34/SBHUD.java
+++ src/main/java/io/github/chaosdave34/SBHUD.java
@@ -1,16 +1,11 @@
 package io.github.chaosdave34;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.InstanceCreator;
 import gg.essential.api.EssentialAPI;
 import io.github.chaosdave34.commands.SBHUDCommand;
 import io.github.chaosdave34.listeners.NetworkListener;
 import io.github.chaosdave34.listeners.PlayerListener;
 import io.github.chaosdave34.listeners.RenderListener;
 import io.github.chaosdave34.utils.Utils;
-import io.github.chaosdave34.utils.gson.GsonInitializableTypeAdapter;
-import io.github.chaosdave34.utils.gson.PatternAdapter;
 import lombok.Getter;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -21,17 +16,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 @Mod(modid = SBHUD.MODID, version = SBHUD.VERSION)
 public class SBHUD {
     public static final String MODID = "sbhud";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.1.0";
     public static final String MOD_NAME = "SBHUD";
 
     @Getter private static SBHUD instance;
@@ -47,17 +38,6 @@ public class SBHUD {
 
     @Getter private final Config config;
     @Getter private ComponentsGui componentsGui;
-
-    @Getter
-    private static final Gson gson = new GsonBuilder()
-            .setPrettyPrinting()
-            .registerTypeAdapter(EnumMap.class, (InstanceCreator<EnumMap>) type -> {
-                Type[] types = (((ParameterizedType) type).getActualTypeArguments());
-                return new EnumMap((Class<?>) types[0]);
-            })
-            .registerTypeAdapterFactory(new GsonInitializableTypeAdapter())
-            .registerTypeAdapter(Pattern.class, new PatternAdapter())
-            .create();
 
     public SBHUD() {
         instance = this;
